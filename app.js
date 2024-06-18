4c1ce228b014e48402d006ef599f8ce0edbf9161
Hrishu Devulapalli <hrishudev@gmail.com>
2024-06-18 09:09:11 -0700

    final changes

--- app.js
+++ app.js
@@ -10,12 +10,23 @@ class Person{
         this.residence = residence;
         this.hobbies = hobbies;
     }
+
+    info(){
+      let info = [this.name, this.pets, this.residence, this.hobbies];
+      return info;
+    }
+
+    soundOff(){
+        console.log("My name is " + this.name + ", don't mess with me if you know whats good for you!")
+    }
     addHobby(hobby) {
       this.hobbies.push(hobby);
     }
 
     removeHobby(startIn) {
-        this.hobbies.splice(startIn,1,null);
+        if(startIn > -1){
+        this.hobbies.splice(startIn,1);
+        }
       }
 
     greeting() {
@@ -30,19 +41,26 @@ class Coder extends Person{
         super(name, pets, residence, hobbies);
         this.occupation = "Full Stack Web Developer";
     }
+
     greeting() {
         console.log("Greetings, my fellow Software engineers.")
+    }
+
+    soundOff(){
+        console.log("My name is " + this.name + ", don't mess with me or i'll hack in your Fortnight account and steal your V-bucks !");
     }
 }
 
 let dude = new Person("Tyrone", 'pitbull', "Chicago", ["C++ ", "Java"] );
 dude.addHobby("lifting weights");
 console.log(dude);
+dude.soundOff()
 
 let nerd = new Coder("Demarcus", 'AI Chatbot', "Mom's Basement", ["League of Legends", "C++ ", "Java"] );
 nerd.removeHobby(1);
 console.log(nerd);
 nerd.greeting();
+nerd.soundOff();
 
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
